166652d96dabdece8d9752c0033a26eda9e219c1
Anders Fogh Eriksen <anders@afogh.dk>
2014-09-25 20:47:30 +0200

    Added documentation

--- AFMobilePayRequestHandler/AFMobilePayRequestHandler.h
+++ AFMobilePayRequestHandler/AFMobilePayRequestHandler.h
@@ -9,8 +9,27 @@
 
 @interface AFMobilePayRequestHandler : NSObject
 
+/**
+ *  Check if MobilePay is installed on device.
+ *
+ *  @return `YES` if MobilePay is installed.
+ */
 + (BOOL)isMobilePayInstalled;
+
+/**
+ *  Send payment with amount to phone number.
+ *
+ *  @param amount Amount to send.
+ *  @param number Phone number to receive money.
+ */
 + (void)sendPayment:(double)amount withPhoneNumber:(NSString *)number;
+
+/**
+ *  Send payment request with amount to phone number.
+ *
+ *  @param amount Amount to request.
+ *  @param number Phone number to request money from.
+ */
 + (void)requestPayment:(double)amount withPhoneNumber:(NSString *)number;
 
 @end
